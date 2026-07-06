88793b24a8fe293144367158e83088883f85a706
JEFFRIN JEBUSON K <jebusonjeffrin@gmail.com>
2026-07-06 06:55:15 +0530

    Complete frontend redesign, fix chart generation bugs, and add architecture report

--- src/main/java/com/expense/controller/ExpenseController.java
+++ src/main/java/com/expense/controller/ExpenseController.java
@@ -23,102 +23,112 @@ import java.util.*;
 
 @Controller
 public class ExpenseController {
-	
+
 	@Autowired
 	private ExpenseService service;
-	
-	
-	//private MyBookListService myBookService;
+
+	// private MyBookListService myBookService;
 	@GetMapping("/")
 	public String homedef() {
 		return "home";
 	}
+
 	@GetMapping("/getchart")
 	public String chart() {
 		return "chart";
 	}
+
 	@GetMapping("/home")
 	public String home() {
 		return "home";
 	}
-	
+
 	@GetMapping("/new_expense")
 	public String Newexpense(Model model) {
 		String message = (String) model.getAttribute("message");
-        model.addAttribute("message", message);
+		model.addAttribute("message", message);
 		return "ExpenseForm";
 	}
-	
+
 	@GetMapping("/available_expenses")
 	public ModelAndView getAllExpenses(Model model) {
-		List<Expense>list=service.getAllExpenses();
+		List<Expense> list = service.getAllExpenses();
 		String message = (String) model.getAttribute("message");
-        model.addAttribute("message", message);
-		//System.out.println(list);
-//		ModelAndView m=new ModelAndView();
-//		m.setViewName("bookList");
-//		m.addObject("book",list);
-		return new ModelAndView("ExpenseList","expense",list);
+		model.addAttribute("message", message);
+		// System.out.println(list);
+		// ModelAndView m=new ModelAndView();
+		// m.setViewName("bookList");
+		// m.addObject("book",list);
+		return new ModelAndView("ExpenseList", "expense", list);
 	}
-	
+
 	@PostMapping("/save")
 	public String addExpense(@ModelAttribute Expense b, Principal p) {
-		String email= p.getName();
-		service.save(b,email);
+		String email = p.getName();
+		service.save(b, email);
 		return "redirect:/available_expenses";
 	}
-//	@GetMapping("/my_books")
-//	public String getMyBooks(Model model)
-//	{
-//		List<MyBookList>list=myBookService.getAllMyBooks();
-//		model.addAttribute("book",list);
-//		return "myBooks";
-//	}
-//	@RequestMapping("/mylist/{id}")
-//	public String getMyList(@PathVariable("id") int id) {
-//		Expense b=service.getBookById(id);
-//		MyBookList mb=new MyBookList(b.getId(),b.getAmount(),b.getCurrency(),b.getDate());
-//		myBookService.saveMyBooks(mb);
-//		return "redirect:/my_books";
-//	}
-	
+	// @GetMapping("/my_books")
+	// public String getMyBooks(Model model)
+	// {
+	// List<MyBookList>list=myBookService.getAllMyBooks();
+	// model.addAttribute("book",list);
+	// return "myBooks";
+	// }
+	// @RequestMapping("/mylist/{id}")
+	// public String getMyList(@PathVariable("id") int id) {
+	// Expense b=service.getBookById(id);
+	// MyBookList mb=new
+	// MyBookList(b.getId(),b.getAmount(),b.getCurrency(),b.getDate());
+	// myBookService.saveMyBooks(mb);
+	// return "redirect:/my_books";
+	// }
+
 	@RequestMapping("/available_expenses/editExpense/{id}")
 	public String editExpense(@PathVariable("id") int id, Model model, Principal p) {
-		String email= p.getName();
-		Expense expense=service.getById(id,email);
-		model.addAttribute("expense",expense);
-		
-		    
+		String email = p.getName();
+		Expense expense = service.getById(id, email);
+		model.addAttribute("expense", expense);
+
 		return "ExpenseFormEdit";
 	}
+
 	@RequestMapping("/deleteExpense/{id}")
-	public String deleteExpense(@PathVariable("id")int id, Principal p) {
-		String email= p.getName();
+	public String deleteExpense(@PathVariable("id") int id, Principal p) {
+		String email = p.getName();
 		service.deleteExpenseByIdAndUserId(id, email);
 		return "redirect:/available_expenses";
 	}
-	@RequestMapping(value="/available_expenses/editExpense/update/{id}", method= {RequestMethod.GET, RequestMethod.PUT})
+
+	@RequestMapping(value = "/available_expenses/editExpense/update/{id}", method = { RequestMethod.GET,
+			RequestMethod.PUT })
 	public String updateExpense(@ModelAttribute Expense expense, Principal p) {
-		String email= p.getName();
+		String email = p.getName();
 		service.save(expense, email);
 		return "redirect:/available_expenses";
 	}
+
 	@GetMapping("/entries")
-    public ModelAndView getFilteredEntries(@RequestParam(required = false)  Integer month, @RequestParam(required = false)  Integer year, Principal p, Model model) {
-		String email= p.getName();
+	public ModelAndView getFilteredEntries(@RequestParam(required = false) Integer month,
+			@RequestParam(required = false) Integer year, Principal p, Model model) {
+		String email = p.getName();
 		List<Expense> filteredEntries = service.getFilteredEntries(month, year);
-	    model.addAttribute("entries", filteredEntries);
-	    return new ModelAndView("ExpenseList","expense",filteredEntries);
-    }
+		model.addAttribute("entries", filteredEntries);
+		return new ModelAndView("ExpenseList", "expense", filteredEntries);
+	}
+
 	@GetMapping("/chart")
-	public String generateExpenseChart(@RequestParam("year") int year, Model model) {
-		        List<Expense> expenses = service.getExpensesByYear(year);
-		        Map<Integer, Double> monthlyTotals = service.calculateMonthlyExpenseTotals(expenses);
+	public String generateExpenseChart(@RequestParam(value = "year", required = false) Integer year, Model model) {
+		if (year == null) {
+			return "chart";
+		}
+		List<Expense> expenses = service.getExpensesByYear(year);
+		Map<Integer, Double> monthlyTotals = service.calculateMonthlyExpenseTotals(expenses);
 
-		        model.addAttribute("year", year);
-		        model.addAttribute("monthlyTotals", monthlyTotals);
+		model.addAttribute("year", year);
+		model.addAttribute("monthlyTotals", monthlyTotals);
 
-	    return "ExpenseChart"; // Return the view name for the chart
+		return "ExpenseChart"; // Return the view name for the chart
 	}
 
 }
--- src/main/java/com/expense/service/ExpenseService.java
+++ src/main/java/com/expense/service/ExpenseService.java
@@ -18,71 +18,76 @@ import com.expense.repository.UserRepository;
 
 @Service
 public class ExpenseService {
-	
+
 	@Autowired
 	private ExpenseRepository bRepo;
-	@Autowired 
+	@Autowired
 	private UserRepository urepo;
-	
+
 	public Expense save(Expense b, String username) {
-		user name= urepo.findByEmail(username);
+		user name = urepo.findByEmail(username);
 		b.setuser(name);
 		return bRepo.save(b);
 	}
-	
-	public List<Expense> getAllExpenses(){
+
+	public List<Expense> getAllExpenses() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	    String username = authentication.getName();
-	    return bRepo.findAllByUserEmail(username);
+		String username = authentication.getName();
+		return bRepo.findAllByUserEmail(username);
 	}
-	
+
 	public Expense getById(int id, String username) {
-		//user name= urepo.findByEmail(username);
+		// user name= urepo.findByEmail(username);
 		return bRepo.findByIdAndUserEmail(id, username);
 	}
+
 	@Transactional
 	public void deleteExpenseByIdAndUserId(int id, String u) {
 		bRepo.deleteByIdAndUserEmail(id, u);
 	}
+
 	public List<Expense> getFilteredEntries(Integer month, Integer year) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	    String username = authentication.getName();
+		String username = authentication.getName();
 		if (month != 0 && year != null) {
-            // Filter entries based on both month and year
-            return bRepo.findByMonthAndYear(month, year,username);
-        } else if (month != 0) {
-            // Filter entries based on month only
-        	//int m = Integer.parseInt(month);
-            return bRepo.findByMonth(month,username);
-        } else if (year != null) {
-            // Filter entries based on year only
-        	//int y = Integer.parseInt(year); 
-            return bRepo.findByYear(year,username);
-        } else if (month == 0 && year == null) {
-            // Filter entries based on both month and year
-        	return bRepo.findAllByUserEmail(username);
-        }else {
-            // No filters specified, return all entries
-            return bRepo.findAllByUserEmail(username);
-        }
-    }
-	 public List<Expense> getExpensesByYear(int year) {
-		 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		    String username = authentication.getName();
-	        return bRepo.findByYear(year,username);
-	    }
-	 public Map<Integer, Double> calculateMonthlyExpenseTotals(List<Expense> expenses) {
-	        Map<Integer, Double> monthlyTotals = new HashMap<>();
+			// Filter entries based on both month and year
+			return bRepo.findByMonthAndYear(month, year, username);
+		} else if (month != 0) {
+			// Filter entries based on month only
+			// int m = Integer.parseInt(month);
+			return bRepo.findByMonth(month, username);
+		} else if (year != null) {
+			// Filter entries based on year only
+			// int y = Integer.parseInt(year);
+			return bRepo.findByYear(year, username);
+		} else if (month == 0 && year == null) {
+			// Filter entries based on both month and year
+			return bRepo.findAllByUserEmail(username);
+		} else {
+			// No filters specified, return all entries
+			return bRepo.findAllByUserEmail(username);
+		}
+	}
 
-	        for (Expense expense : expenses) {
-	        	LocalDate date = expense.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		        int month = date.getMonthValue();
-	            double amount = expense.getAmount();
+	public List<Expense> getExpensesByYear(int year) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		return bRepo.findByYear(year, username);
+	}
 
-	            monthlyTotals.put(month, monthlyTotals.getOrDefault(month, 0.0) + amount);
-	        }
+	public Map<Integer, Double> calculateMonthlyExpenseTotals(List<Expense> expenses) {
+		Map<Integer, Double> monthlyTotals = new HashMap<>();
 
-	        return monthlyTotals;
-	    }
+		for (Expense expense : expenses) {
+			LocalDate date = new java.util.Date(expense.getDate().getTime()).toInstant().atZone(ZoneId.systemDefault())
+					.toLocalDate();
+			int month = date.getMonthValue();
+			double amount = expense.getAmount();
+
+			monthlyTotals.put(month, monthlyTotals.getOrDefault(month, 0.0) + amount);
+		}
+
+		return monthlyTotals;
+	}
 
 }
